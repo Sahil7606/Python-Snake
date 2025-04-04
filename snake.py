b8cd16ca19db1874c3073c14b4075120d6cd49b2
Sahil R. Patel <srp5973@psu.edu>
2025-04-04 10:52:53 -0400

    Set up curses library

---
+++ snake.py
@@ -0,0 +1,27 @@
+import curses 
+
+class Snake:
+    """
+    Attributes:
+        head_position (tuple)
+        length (int)
+    """
+    pass
+
+class Apple: 
+    """
+    Attributes:
+        postition (tuple)
+    """
+    pass
+ 
+class Game:
+    """
+    Attributes:
+        height (int)
+        width (int)
+        board_matrix (lst[lst])
+    """
+    pass
+
+stdscr = curses.initscr()
