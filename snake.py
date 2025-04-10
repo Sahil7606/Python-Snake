3f04cb21bcc48382223f00a3b110d51898b6cca8
Sahil R. Patel <srp5973@psu.edu>
2025-04-09 21:18:17 -0400

    SNAKE MOVES NOW!!!!!

--- snake.py
+++ snake.py
@@ -2,7 +2,11 @@ import time
 import curses 
 from curses import wrapper
 
-# Tomorrow: get the snake to move
+# Tomorrow: get the snake to move 
+class Snake_Node:
+    def __init__(self, position: tuple, next = None):
+        self.position = position
+        self.next = next
 
 class Snake:
     """
@@ -10,19 +14,42 @@ class Snake:
         head_position (tuple)
         length (int)
         speed (float)
-        body (set)
+        direction (str)
     """
     def __init__(self, row, col):
-        self.head_position = (row, col)
-        self.length = 2
-        self.speed = 0.2
-        self.body = set((row, col - 1)) # Creates body part behind head
+        self.head = Snake_Node((row, col))
+        self.tail = self.head
+        self.direction = (1, 0)
+        self.length = 1
 
-    def setDirection(self, direction):
-        pass
+        self.__growPending = False
 
-    def grow(self, amount = 1):
-        pass
+    def __enqueue_head(self, position: tuple):
+        new_node = Snake_Node(position)
+        self.head.next = new_node
+        self.head = self.head.next
+
+        self.length += 1
+
+    def __dequeue_tail(self):
+        removed_item = self.tail
+        self.tail = self.tail.next
+        removed_item = None
+
+        self.length -= 1
+
+    def setDirection(self, direction: tuple):
+        self.direction = direction
+
+    def grow(self):
+        self.__growPending = True
+
+    def move(self):
+        next_pos = (self.head.position[0] + self.direction[0], self.head.position[1] + self.direction[1])
+        self.__enqueue_head(next_pos)
+        
+        if not self.__growPending:
+            self.__dequeue_tail()
 
 class Apple: 
     """
@@ -46,7 +73,7 @@ class Game:
         self.walls = None
         self.snake = None
 
-    def renderWalls(self, stdscr):  # Do I really need this?
+    def __renderWalls(self, stdscr):
         # Top boundary
         stdscr.addstr(0, 0, '+' + ('-' * (self.width - 2)) + '+')
         
@@ -60,7 +87,7 @@ class Game:
         
         stdscr.refresh()
 
-    def createWalls(self):
+    def __createWalls(self):
         self.walls = set()
         for i in range(self.height):
             if i == 0 or i == self.height - 1:
@@ -71,22 +98,60 @@ class Game:
                 self.walls.add((i, self.width - 1))
 
     def initializeGame(self, stdscr):
-        self.renderWalls(stdscr)
-        self.createWalls()
+        # Hides cursor and clears screen
+        curses.curs_set(0)
+        stdscr.clear()
+
+        self.__renderWalls(stdscr)
+        self.__createWalls()
 
         self.snake = Snake(self.height // 2, 5)
-        stdscr.addstr(self.snake.head_position[0], self.snake.head_position[1], 'X')
-        stdscr.addstr(self.snake.head_position[0], self.snake.head_position[1] - 1, 'O')
 
-    def startGame(self):
-        pass
+        stdscr.refresh()
+
+    def startGame(self, stdscr):
+        stdscr.nodelay(True)
+        while True:
+            # Clear the previous frame
+            stdscr.clear()
+            self.__renderWalls(stdscr)
+            
+            # Handle input
+            key = stdscr.getch()
+            if key == curses.KEY_DOWN:
+                self.snake.setDirection((1, 0))  # DOWN increases row
+            elif key == curses.KEY_UP:
+                self.snake.setDirection((-1, 0))  # UP decreases row
+            elif key == curses.KEY_LEFT:
+                self.snake.setDirection((0, -1))
+            elif key == curses.KEY_RIGHT:
+                self.snake.setDirection((0, 1))
+
+            # Move snake
+            self.snake.move()
+            
+            # Draw snake
+            current = self.snake.tail
+            while current:
+                stdscr.addstr(current.position[0], current.position[1], 'O') 
+                current = current.next
+            
+            stdscr.refresh()
+            time.sleep(0.1)  # Reduced sleep time for smoother movement
+
 
 def main(stdscr):
-    curses.curs_set(0)  # Hide cursor
-    stdscr.clear()
-    game = Game(21, 50)  # Wider board (20 columns) for safety
+    game = Game(21, 50)
     game.initializeGame(stdscr)
-    stdscr.refresh()
-    stdscr.getch()  # Wait for keypress
 
+    # Gets user input to start game
+    key = stdscr.getch()
+    while key != 10 and key != 27:
+        key = stdscr.getch()
+
+    if key == 27:
+        return
+    
+    game.startGame(stdscr)
+    
 wrapper(main)
